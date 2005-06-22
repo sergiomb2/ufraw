c0245e1816326a418e4a387fb9e6625e46ef682b
udifuchs <udifuchs>
2005-06-22 17:14:46 +0000

    - Makefile.am now supports automake-1.5 or heigher. - autogen.sh tries to find the right automake. ('automake' itself might point to an older version.)

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
-aclocal
+# Generate all autoconf/automake files.
+# First look for the heighest version of automake available.
+AUTOMAKE=automake-1.9
+ACLOCAL=aclocal-1.9
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+    AUTOMAKE=automake-1.8
+    ACLOCAL=aclocal-1.8
+}
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+    AUTOMAKE=automake-1.7
+    ACLOCAL=aclocal-1.7
+}
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+    AUTOMAKE=automake-1.6
+    ACLOCAL=aclocal-1.6
+}
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+    AUTOMAKE=automake-1.5
+    ACLOCAL=aclocal-1.5
+}
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
+    echo "You must have automake-1.5 or heigher"
+    exit 1
+}
+$ACLOCAL
 autoconf
 autoheader
-automake --foreign --add-missing --copy
-
+$AUTOMAKE --foreign --add-missing --copy
