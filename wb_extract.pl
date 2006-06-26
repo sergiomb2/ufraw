b4bd4b92554b583e2796f392622ce4e5558bf9f1
cbennett <cbennett>
2006-06-26 16:40:17 +0000

    Added support in wb_extract.pl for D70 white balance values, which are in different Exif fields than the D200.

--- wb_extract.pl
+++ wb_extract.pl
@@ -12,18 +12,24 @@ if (@ARGV < 1) {
 #my ($lastmul1, $lastmul2, $lastmul3) = (0,0,0);
 
 for my $file (@ARGV) {
-  my ($make, $model, $wbname, $wbfinetune, $mul1, $mul2, $mul3);
+  my ($make, $model, $wbname, $wbfinetune, $mulred, $mulgreen, $mulblue);
 
-  open(EXIFTOOL, "exiftool -s -t -Model -ColorBalance1 -WhiteBalance -WhiteBalanceFineTune $file|") 
+  $mulgreen = 1;  # default value for green balance
+
+  open(EXIFTOOL, "exiftool -s -t -Model -ColorBalance1 -RedBalance -BlueBalance -WhiteBalance -WhiteBalanceFineTune $file|") 
     or die "can't open $file: $!";
   while (my $line = <EXIFTOOL>) {
     $line =~ /([^\t]+)\t(.*)/ or next;
     my ($field, $value) = ($1, $2);
     if ($field eq "Model") {
       ($make, $model) = split(/ +/, $value);
-    } elsif ($field eq "ColorBalance1") {
+    } elsif ($field eq "ColorBalance1") {   # Field for D200
       my $mul_unknown;
-      ($mul1, $mul3, $mul2, $mul_unknown) = split(/ +/, $value);
+      ($mulred, $mulblue, $mulgreen, $mul_unknown) = split(/ +/, $value);
+    } elsif ($field eq "RedBalance") {   # Field for D70 (red)
+      $mulred = $value;
+    } elsif ($field eq "BlueBalance") {   # Field for D70 (blue)
+      $mulblue = $value;
     } elsif ($field eq "WhiteBalance") {
       $wbname = $value;
     } elsif ($field eq "WhiteBalanceFineTune") {
@@ -32,14 +38,14 @@ for my $file (@ARGV) {
   }
   close EXIFTOOL;
 
-  # Fix names for consistency across Nikons (D70 and D2X use "Direct sunlight")
+  # Fix names for consistency across Nikon cameras (D70 and D2X use "Direct sunlight")
   $wbname =~ s/^Sunny$/Direct sunlight/;
   
   # Format and print the line
   my $result;
   $result = sprintf "  { \"%s\", \"%s\", \"%s\", %d,", $make, $model, $wbname, $wbfinetune;
   $result .= " " while length($result) < 48;
-  $result .= "{ $mul1, $mul2, $mul3, 0 } },";
+  $result .= "{ $mulred, $mulgreen, $mulblue, 0 } },";
 
   # debug: add deltas
   #$result .= sprintf "/* dm1=%.4f dm2=%.4f dm3=%.4f */", $mul1-$lastmul1, $mul2-$lastmul2, $mul3-$lastmul3;
@@ -58,3 +64,4 @@ for my $file (@ARGV) {
 #ColorBalance1   1.234375 2.136719 1 1
 #WhiteBalance    Incandescent
 #WhiteBalanceFineTune    -3
+
