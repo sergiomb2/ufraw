b121ca56bd1c6075cc0f93a9a7b02f57657501fa
udifuchs <udifuchs>
2006-08-05 21:59:30 +0000

    Reconize Sony's *.arw file type.

--- ufraw_routines.c
+++ ufraw_routines.c
@@ -97,7 +97,7 @@ char *uf_markup_buf(char *buffer, const char *format, ...)
     }
 }
 
-const char raw_ext[]= "bay,bmq,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,jpg,k25,"
+const char raw_ext[]= "arw,bay,bmq,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,jpg,k25,"
 	"kdc,mdc,mos,mrw,nef,orf,pef,pxn,raf,raw,rdc,sr2,srf,sti,tif,ufraw,x3f";
 
 const char *file_type[] = { ".ppm", ".ppm", ".tif", ".tif", ".jpg" };
