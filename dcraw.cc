a4bcf4c778efaf3256a32c9d76714d2374154658
nkbj <nkbj>
2006-08-25 06:09:41 +0000

    dcraw modified 8.32 (1.343).

--- dcraw.cc
+++ dcraw.cc
@@ -15,15 +15,15 @@
    license. Naturaly, the GPL license applies only to this derived
    work.
 
-   $Revision: 1.342 $
-   $Date: 2006/08/20 05:14:23 $
+   $Revision: 1.343 $
+   $Date: 2006/08/24 20:24:51 $
  */
 
 #ifdef HAVE_CONFIG_H /*For UFRaw config system - NKBJ*/
 #include "config.h"
 #endif
 
-#define DCRAW_VERSION "8.31"
+#define DCRAW_VERSION "8.32"
 
 //#define _GNU_SOURCE /*UF*/
 #define _USE_MATH_DEFINES
@@ -195,7 +195,7 @@ int CLASS fc (int row, int col)
 }
 
 #ifndef HAVE_MEMMEM
-char *my_memmem (char *haystack, size_t haystacklen,
+char *memmem (char *haystack, size_t haystacklen,
 	      char *needle, size_t needlelen)
 {
   char *c;
@@ -204,7 +204,6 @@ char *my_memmem (char *haystack, size_t haystacklen,
       return c;
   return NULL;
 }
-#define memmem my_memmem
 #endif
 
 #ifndef DCRAW_NOMAIN
@@ -4682,7 +4681,7 @@ void CLASS ciff_block_1030()
  */
 void CLASS parse_ciff (int offset, int length)
 {
-  int tboff, nrecs, i, c, type, len, save, wbi=-1;
+  int tboff, nrecs, c, type, len, save, wbi=-1;
   ushort key[] = { 0x410, 0x45f3 };
 
   fseek (ifp, offset+length-4, SEEK_SET);
@@ -4690,7 +4689,7 @@ void CLASS parse_ciff (int offset, int length)
   fseek (ifp, tboff, SEEK_SET);
   nrecs = get2();
   if (nrecs > 100) return;
-  for (i=0; i < nrecs; i++) {
+  while (nrecs--) {
     type = get2();
     len  = get4();
     save = ftell(ifp) + 4;
@@ -4719,10 +4718,12 @@ void CLASS parse_ciff (int offset, int length)
     }
     if (type == 0x102a) {
       iso_speed = pow (2, (get4(),get2())/32.0 - 4) * 50;
-      aperture  = pow (2, (get2(),get2())/64.0);
+      aperture  = pow (2, (get2(),(short)get2())/64.0);
       shutter   = pow (2,-((short)get2())/32.0);
       wbi = (get2(),get2());
       if (wbi > 17) wbi = 0;
+      fseek (ifp, 32, SEEK_CUR);
+      if (shutter > 1e6) shutter = get2()/10.0;
     }
     if (type == 0x102c) {
       if (get2() > 512) {		/* Pro90, G1 */
