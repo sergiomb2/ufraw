5a9d8a88dc0eb8095395d8216ea257bc53d55f9c
udifuchs <udifuchs>
2006-09-15 18:39:59 +0000

    Fix crash cause by int/unsigned tricks.

--- dcraw.cc
+++ dcraw.cc
@@ -868,11 +868,11 @@ void CLASS adobe_copy_pixel (int row, int col, ushort **rp)
       r = row + fuji_width - 1 - (col >> 1);
       c = row + ((col+1) >> 1);
     }
-    if (r < height && c < width)
+    if (r < height && r>=0 && c < width && c>=0)
       BAYER(r,c) = **rp < 0x1000 ? curve[**rp] : **rp;
     *rp += 1 + fuji_secondary;
   } else {
-    if (r < height && c < width)
+    if (r < height && r>=0 && c < width && c>=0)
       for (c=0; c < tiff_samples; c++)
 	image[row*width+col][c] = (*rp)[c] < 0x1000 ? curve[(*rp)[c]]:(*rp)[c];
     *rp += tiff_samples;
