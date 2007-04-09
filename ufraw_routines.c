38694c80e2c95a0f097ea8eb6d82cff47dcdbf0a
nkbj <nkbj>
2007-04-09 07:18:32 +0000

    Revert latest change.

--- ufraw_routines.c
+++ ufraw_routines.c
@@ -142,9 +142,9 @@ char *uf_markup_buf(char *buffer, const char *format, ...)
     }
 }
 
-const char raw_ext[]= "arw,bay,bmq,bz2,cr2,crw,cs1,dc2,dcr,dng,erf,fff,gz,hdr,"
-	"ia,jpg,k25,kc2,kdc,mdc,mos,mrw,nef,orf,pef,pxn,raf,raw,rdc,sr2,srf,"
-	"sti,tif,ufraw,x3f";
+const char raw_ext[]= "arw,bay,bmq,cr2,crw,cs1,dc2,dcr,dng,erf,fff,hdr,ia,jpg,"
+	"k25,kc2,kdc,mdc,mos,mrw,nef,orf,pef,pxn,raf,raw,rdc,sr2,srf,sti,tif,"
+	"ufraw,x3f";
 
 const char *file_type[] = { ".ppm", ".ppm", ".tif", ".tif", ".jpg",
 	".embedded.jpg", ".embedded.png" };
