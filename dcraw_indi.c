8317b5480f6134d4a1225116b54df5476ac46ba7
nkbj <nkbj>
2007-06-09 06:20:56 +0000

    More updates for ppg_interpolate_INDI().

--- dcraw_indi.c
+++ dcraw_indi.c
@@ -471,7 +471,7 @@ void CLASS ppg_interpolate_INDI(ushort (*image)[4], const unsigned filters,
         const int width, const int height, const int colors, void *dcraw)
 {
   int gr[4], dir[5] = { 1, width, -1, -width, 1 };
-  int row, col, avg, pat, diff[2], guess[2], c, d, i;
+  int row, col, avg, diff[2], guess[2], c, d, i;
   static const short sort[] = { 0,2,1,3,0,1,2,3 };
   ushort (*pix)[4];
 
@@ -485,21 +485,17 @@ void CLASS ppg_interpolate_INDI(ushort (*image)[4], const unsigned filters,
       for (avg=i=0; i < 4; i++)
 	avg += gr[i] = pix[dir[i]][1] << 2;
       avg >>= 2;
-      for (pat=i=0; i < 4; i++)
-	pat = pat << 1 | (gr[i] > avg);
-      if (pat & 8) pat ^= 15;
       for (i=0; i < 8; i+=2)
 	if (gr[sort[i]] > gr[sort[i+1]])
 	  SWAP(gr[sort[i]],gr[sort[i+1]])
-      if (pat == 0 || pat == 3 || pat == 6)
-	for (d=0; d < 4; d++) {
-	  for (i=-2; i < 2; i++)
-	    if (pix[i*dir[d] + (i+1)*dir[d+1]][1] <= avg) break;
-	  if (i == 2) {
-	    pix[0][1] = (gr[1]+gr[2]) >> 3;
-	    goto next_pixel;
-	  }
+      for (d=0; d < 4; d++) {
+	for (i=-2; i < 2; i++)
+	  if (pix[i*dir[d] + (i+1)*dir[d+1]][1] <= avg) break;
+	if (i == 2) {
+	  pix[0][1] = (gr[1]+gr[2]) >> 3;
+	  goto next_pixel;
 	}
+      }
       for (i=0; (d=dir[i]) > 0; i++) {
 	guess[i] = (pix[-d][1] + pix[0][c] + pix[d][1]) * 2
 		      - pix[-2*d][c] - pix[2*d][c];
