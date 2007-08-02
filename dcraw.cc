c744084d3140662f51e398922124c5afd2d1dc6c
nkbj <nkbj>
2007-08-02 04:17:55 +0000

    dcraw original 8.77 (1.390) updated.

--- dcraw.cc
+++ dcraw.cc
@@ -19,7 +19,7 @@
    homepage qualifies as "full source code".
 
    $Revision: 1.390 $
-   $Date: 2007/07/30 15:43:07 $
+   $Date: 2007/08/01 17:39:28 $
  */
 
 #define VERSION "8.77"
@@ -4468,7 +4468,6 @@ void CLASS get_timestamp (int reversed)
   char str[20];
   int i;
 
-  if (timestamp) return;
   str[19] = 0;
   if (reversed)
     for (i=19; i--; ) str[i] = fgetc(ifp);
@@ -4497,8 +4496,8 @@ void CLASS parse_exif (int base)
       case 33434:  shutter = getrat();			break;
       case 33437:  aperture = getrat();			break;
       case 34855:  iso_speed = get2();			break;
-      case 36867:  get_timestamp(0);			break;
-      case 36868:  if (!timestamp) get_timestamp(0);	break;
+      case 36867:
+      case 36868:  get_timestamp(0);			break;
       case 37377:  if ((expo = -getrat()) < 128)
 		     shutter = pow (2, expo);		break;
       case 37378:  aperture = pow (2, getrat()/2);	break;
