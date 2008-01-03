a11bc9908ca73603bd7eec9ac5aac245fb597d27
nkbj <nkbj>
2008-01-03 17:30:11 +0000

    Fix for localized exiv2 values. Patch by Andrew Zabolotny.

--- ufraw_exiv2.cc
+++ ufraw_exiv2.cc
@@ -25,6 +25,25 @@ extern "C" {
 #include <sstream>
 #include <cassert>
 
+/**
+ * Helper function to copy a string to a buffer, converting it from
+ * current locale (in which exiv2 often returns strings) to UTF-8.
+ */
+static void strlcpy_to_utf8 (char *dest, size_t dest_max, Exiv2::ExifData::iterator pos)
+{
+	std::stringstream str;
+	str << *pos;
+
+	char *s = g_locale_to_utf8 (str.str ().c_str (), str.str ().length (),
+				    NULL, NULL, NULL);
+	if (s) {
+		g_strlcpy (dest, s, dest_max);
+		g_free (s);
+	}
+	else
+		g_strlcpy (dest, str.str ().c_str (), dest_max);
+}
+
 extern "C" int ufraw_exif_from_exiv2(ufraw_data *uf)
 {
     /* Redirect exiv2 errors to a string buffer */
@@ -107,120 +126,87 @@ try {
     /* Read shutter time */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.ExposureTime")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->shutterText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->shutterText, max_name, pos);
+	uf->conf->shutter = pos->toFloat ();
     } else if ( (pos=exifData.findKey(
 		    Exiv2::ExifKey("Exif.Photo.ShutterSpeedValue")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->shutterText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->shutterText, max_name, pos);
+	uf->conf->shutter = 1.0 / pos->toFloat ();
     }
     /* Read aperture */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.FNumber")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->apertureText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->apertureText, max_name, pos);
+	uf->conf->aperture = pos->toFloat ();
     } else if ( (pos=exifData.findKey(
 		    Exiv2::ExifKey("Exif.Photo.ApertureValue")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->apertureText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->apertureText, max_name, pos);
+	uf->conf->aperture = pos->toFloat ();
     }
     /* Read ISO speed */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.ISOSpeedRatings")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey(
 			"Exif.CanonSi.ISOSpeed"))) != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Nikon1.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Nikon2.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Nikon3.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(
 		    Exiv2::ExifKey("Exif.MinoltaCsNew.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(
 		    Exiv2::ExifKey("Exif.MinoltaCsOld.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(
 		    Exiv2::ExifKey("Exif.MinoltaCs5D.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey(
 			"Exif.MinoltaCs7D.ISOSpeed")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->isoText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->isoText, max_name, pos);
     }
     /* Read focal length */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.FocalLength")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->focalLenText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->focalLenText, max_name, pos);
+	uf->conf->focal_len = pos->toFloat ();
     }
     /* Read focal length in 35mm equivalent */
     if ( (pos=exifData.findKey(Exiv2::ExifKey(
 		"Exif.Photo.FocalLengthIn35mmFilm")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->focalLen35Text, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->focalLen35Text, max_name, pos);
     }
     /* Read lens name */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Nikon3.Lens")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->lensText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->lensText, max_name, pos);
     } else if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Canon.0x0095")))
 		!= exifData.end() ) {
-	std::stringstream str;
-	str << *pos;
-	g_strlcpy(uf->conf->lensText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->lensText, max_name, pos);
     }
     /* Read flash mode */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.Flash")))
 	    != exifData.end() ) {
-	std::stringstream str;
-       	str << *pos;
-	g_strlcpy(uf->conf->flashText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->flashText, max_name, pos);
     }
     /* Read White Balance Setting */
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Photo.WhiteBalance")))
 	    != exifData.end() ) {
-	std::stringstream str;
-	str << *pos;
-	g_strlcpy(uf->conf->whiteBalanceText, str.str().c_str(), max_name);
+	strlcpy_to_utf8(uf->conf->whiteBalanceText, max_name, pos);
     }
     std::cerr.rdbuf(savecerr);
     ufraw_message(UFRAW_SET_LOG, "%s\n", stderror.str().c_str());
