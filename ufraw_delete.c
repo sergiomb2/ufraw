8e3fcabe7166afb60a095c70e18753bd9725ded4
nkbj <nkbj>
2008-06-20 15:00:13 +0000

    Update copyright information.

--- ufraw_delete.c
+++ ufraw_delete.c
@@ -2,7 +2,7 @@
  * UFRaw - Unidentified Flying Raw converter for digital camera images
  *
  * ufraw_delete.c - The GUI for choosing what files to delete.
- * Copyright 2004-2007 by Udi Fuchs
+ * Copyright 2004-2008 by Udi Fuchs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2
