176357f08236ef3ae54d1e9997891ee4fdf5833d
nkbj <nkbj>
2008-07-03 15:45:06 +0000

    Update copyright information.

--- uf_gtk.cc
+++ uf_gtk.cc
@@ -2,7 +2,7 @@
  * UFRaw - Unidentified Flying Raw converter for digital camera images
  *
  * uf_gtk.cc - gtk compatibility layer
- * Copyright 2004-2007 by Udi Fuchs
+ * Copyright 2004-2008 by Udi Fuchs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- ufraw_saver.c
+++ ufraw_saver.c
@@ -2,7 +2,7 @@
  * UFRaw - Unidentified Flying Raw converter for digital camera images
  *
  * ufraw_saver.c - The GUI file saver.
- * Copyright 2004-2007 by Udi Fuchs
+ * Copyright 2004-2008 by Udi Fuchs
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
