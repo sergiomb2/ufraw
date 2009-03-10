0722527a230bc1634eb4149ae7768404b228c0a9
nkbj <nkbj>
2009-03-10 03:45:32 +0000

    dcraw modified 8.93 (1.421). Changelog: Pentax K2000/K-m Huffman tree must be read from the MakerNote. The Kodak 620x and 720x use the filter pattern YMCY, not GRBG.

--- dcraw.h
+++ dcraw.h
@@ -145,6 +145,7 @@ void canon_sraw_load_raw();
 void adobe_copy_pixel (int row, int col, ushort **rp);
 void adobe_dng_load_raw_lj();
 void adobe_dng_load_raw_nc();
+void pentax_tree();
 void pentax_k10_load_raw();
 void nikon_compressed_load_raw();
 int nikon_is_compressed();
