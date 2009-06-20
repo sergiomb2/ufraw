565fae306b37ac0274928de7ac0e6670da035ee5
nkbj <nkbj>
2009-06-20 03:00:12 +0000

    dcraw modified 8.95 (1.425). Changelog: Support the Phase One P45+.

--- dcraw.cc
+++ dcraw.cc
@@ -11,8 +11,8 @@
    This is a adaptation of Dave Coffin's original dcraw.c to C++.
    It can work as either a command-line tool or called by other programs.
 
-   $Revision: 1.424 $
-   $Date: 2009/06/09 05:43:45 $
+   $Revision: 1.425 $
+   $Date: 2009/06/19 21:38:38 $
  */
 
 #ifdef HAVE_CONFIG_H /*For UFRaw config system - NKBJ*/
@@ -5381,6 +5381,7 @@ void CLASS parse_tiff (int base)
 	max_samp = tiff_ifd[i].samples;
     if (max_samp > 3) max_samp = 3;
     if ((tiff_ifd[i].comp != 6 || tiff_ifd[i].samples != 3) &&
+	(tiff_ifd[i].width | tiff_ifd[i].height) < 0x10000 &&
 	tiff_ifd[i].width*tiff_ifd[i].height > raw_width*raw_height) {
       raw_width     = tiff_ifd[i].width;
       raw_height    = tiff_ifd[i].height;
