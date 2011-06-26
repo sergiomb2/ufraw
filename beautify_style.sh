02bc2df0c6c2d9d1892bd16a58e319d81e79559d
nkbj <nkbj>
2011-06-26 07:02:06 +0000

    Also beautify wb_presets.c.

--- beautify_style.sh
+++ beautify_style.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# this uses astyle to standardize our indentation/braces etc
+# This uses astyle to standardize our indentation/braces etc.
 
 SOURCES=$(find . -name "*.c" -or -name "*.cc" -or -name "*.h" | grep -v dcraw.cc | grep -v wb_presets.c)
 
 for i in $SOURCES
 do
-  astyle --style=k/r --indent=spaces=4 --indent-switches --pad-oper --pad-header --unpad-paren < $i > UFRawBeautifiStyle.tmp
-  mv UFRawBeautifiStyle.tmp $i
+  astyle --style=k/r --indent=spaces=4 --indent-switches --pad-oper --pad-header --unpad-paren < $i > UFRawBeautifyStyle.tmp
+  mv UFRawBeautifyStyle.tmp $i
 done
+
+astyle --style=k/r --indent=spaces=2 --indent-switches < wb_presets.c > UFRawBeautifyStyle.tmp
+mv UFRawBeautifyStyle.tmp wb_presets.c
