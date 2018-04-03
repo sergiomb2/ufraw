3c5b1df48d9e3a83bc4ee086eb71fb95553b71f8
rodrigo <rodrigo@osorio.me>
2018-04-03 20:25:14 +0200

    Don't use gimp-remote since gimp 2.4 already includes gimp-remote functionality Imported from Debian

--- ufraw_conf.c
+++ ufraw_conf.c
@@ -138,7 +138,7 @@ const conf_data conf_default = {
 #elif HAVE_GIMP_2_4
     "gimp", /* remoteGimpCommand */
 #else
-    "gimp-remote", /* remoteGimpCommand */
+    "gimp", /* remoteGimpCommand */
 #endif
 
     /* EXIF data */
