33e35808b95225545286b98d0b6bec2c1776e837
Sérgio M. Basto <sergio@serjux.com>
2021-04-25 00:51:34 +0100

    GCC 10/11 warnings eliminated
    from https://github.com/LibRaw/LibRaw/commit/e8d5a25e4889e7d5006f6eba12d11ab1f6b69136

--- dcraw.cc
+++ dcraw.cc
@@ -831,7 +831,7 @@ int CLASS canon_has_lowbits()
 
   fseek (ifp, 0, SEEK_SET);
   fread (test, 1, sizeof test, ifp);
-  for (i=540; i < sizeof test - 1; i++)
+  for (i=540; i < int(sizeof test - 1); i++)
     if (test[i] == 0xff) {
       if (test[i+1]) return 1;
       ret=0;
